f0c18801b3a788730d7a83fa7c27976d52739726
PaulFel123 <if22b151@technikum-wien.at>
2024-04-28 15:02:57 +0200

    Added ability to collect coins with soundeffect

---
+++ Assets/Scripts/CoinBehaviour.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+public class CoinBehaviour : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            _audioSource.Play();
+            Destroy(gameObject, 0.2f);
+        }
+    }
+}
