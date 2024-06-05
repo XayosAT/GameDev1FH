6187becbf9fedbee125c2b20a10e8a3959cc1fed
Taharium <if22b207@technikum-wien.at>
2024-06-05 12:55:11 +0200

    added saw and platform animator

--- Assets/Scripts/Obstacles/Trampoline.cs
+++ Assets/Scripts/Obstacles/Trampoline.cs
@@ -1,11 +1,28 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Trampoline : MonoBehaviour, IObstacle
 {
     public float jumpingPower = 15f;
+    private Animator _animator;
 
-    public void InteractWithObstacle(GameObject player)
-    {
+    private void Start() {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void InteractWithObstacle(GameObject player) {
+        OnCollisionEnter2D();
         player.GetComponent<Rigidbody2D>().velocity = new Vector2(player.GetComponent<Rigidbody2D>().velocity.x, jumpingPower);
+    }
+
+    private void OnCollisionEnter2D() {
+        _animator.SetBool("IsJumping", true);
+        StartCoroutine(SetBackToIdle());
+    }
+
+    private IEnumerator SetBackToIdle() {
+        yield return new WaitForSeconds(_animator.GetCurrentAnimatorStateInfo(0).length);
+        _animator.SetBool("IsJumping", false);
     }
 }
