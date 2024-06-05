af24e13856265c922779f8101801e4ba71ec45c2
Taharium <if22b207@technikum-wien.at>
2024-06-05 11:27:32 +0200

    added lost scripts
    .

---
+++ Assets/Scripts/Obstacles/HorizontalMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour, IMovement
+{
+    public float speed = 3f;
+    private bool movingRight = true;
+    private Vector3 startPos;
+    public float distance;
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float leftBoundary = startPos.x - distance;
+
+        if (transform.position.x >= startPos.x)
+            movingRight = false;
+        else if (transform.position.x <= leftBoundary)
+            movingRight = true;
+
+        transform.Translate((movingRight ? Vector3.right : Vector3.left) * (Time.deltaTime * speed));
+    }
+}
